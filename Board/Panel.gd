60fdb3ffa65ec554748579cef2735c2e9624193d
Mark van der Veen <mark.vanderveen@kro-ncrv.nl>
2025-07-02 15:31:11 +0200

    Added highlighting of squares

--- Board/Panel.gd
+++ Board/Panel.gd
@@ -2,11 +2,17 @@ extends Panel
 
 var boardSize = Vector2(3, 3)
 var selected = Vector2(0, 0)
+var hover = false
+
+@onready var highlight = $Highlight
+
+func _ready() -> void:
+	highlight.size = size / boardSize
+	highlight.position = Vector2(0, 0)
 
 func _gui_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		selected = (get_local_mouse_position() / size * boardSize)
 		selected = Vector2(int(selected.x), int(selected.y))
+		highlight.position = highlight.size * selected
 		print(selected)
-
-	
--- Deck/Card/Card.gd
+++ Deck/Card/Card.gd
@@ -4,8 +4,12 @@ extends Control
 var values = {
 	"Rathalos": [4, 2, 3, 2],
 	"Rathian": [3, 2, 4, 2],
-	"Nargacuga": [1, 3, 2, 3],
-	"Tigrex": [4, 2, 1, 2]
+	"Nargacuga": [2, 4, 2, 4],
+	"Tigrex": [5, 3, 1, 3],
+	"Brachydios": [4, 5, 1, 5],
+	"Lagiacrus": [3, 2, 4, 2],
+	"Seregios": [3, 3, 2, 3],
+	"Zinogre": [2, 3, 3, 3]
 }
 
 @onready var directions = {
